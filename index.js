d2b1a654f4f4cac41c5daf372cbef59440b05992
Mohan Sai <potlamohansai@gmail.com>
2022-09-07 17:12:31 +0530

    day 8 practice

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+function factorialOfPrime(lowerRange, upperRange) {
 
-let result = Math.trunc(-3.2);
+    //Logic to print Prime Numbers
+    let primeNumbers =[];
+    for (j=lowerRange; j <= upperRange;j++) {
+        let count = 0;
+        for (let i= 1 ; i <= upperRange ; i++) {
+            if(j%i == 0) {
+                count++;
+            }
+        }
+        if (count ==2) {
+            primeNumbers.push(j)
+        }
+    }
+
+    // Logic to printt Factorial
+
+    for(let num of primeNumbers){
+        let fact =1;
+        for(let x=1; x<=num; x++){
+            fact = fact * x;
+        }
+        console.log (`Factorial of ${num}! is ${fact}.`);
+    }
+    return (`List of Prime Numbers between ${lowerRange} and ${upperRange} is [${primeNumbers}]`);
+    
+}
+
+
+//function checking
+
+let result = factorialOfPrime(1,20);
 console.log(result);
